aaaeec436574aeb8f23cc0ea728557d8e88716ae
Krishna <kmr614@gmail.com>
2022-10-18 17:04:51 -0400

    test edit

--- Goals_For.py
+++ Goals_For.py
@@ -7,6 +7,7 @@ File contains the Artifical Neural Network machine learning model and applying i
 #import the libaries
 import numpy as np
 import pandas as pd
+from sklearn.cluster import k_means
 import tensorflow as tf
 
 tf.__version__
@@ -20,9 +21,4 @@ take the opponent and goals against and the outcome guess if how many goals are 
 
 '''
 dataset = pd.read_csv('/home/ish/Projects/Python_Projects/Pens_Betting/Pens Game Data/Pens_Data.csv')
-
-
-
-
-
 
