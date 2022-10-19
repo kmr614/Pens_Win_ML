6783334187e3b3901d618060774f8382d9ba41cb
Krishna <kmr614@gmail.com>
2022-10-19 13:48:44 -0400

    clean up

--- Goals_For.py
+++ Goals_For.py
@@ -11,6 +11,7 @@ import pandas as pd
 import math
 from sklearn.cluster import k_means
 import tensorflow as tf
+from function_lib import *
 
 tf.__version__
 
@@ -54,7 +55,7 @@ sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.fit_transform(x_test)
 
-
+#Creating the ANN 
 ann = tf.keras.models.Sequential()
 
 ann.add(tf.keras.layers.Dense(units = 6, activation = 'relu'))
@@ -65,27 +66,25 @@ ann.add(tf.keras.layers.Dense(units = 1, activation = 'relu'))
 
 ann.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy'])
 
-ann.fit(x_train, y_train, batch_size = 32, epochs = 100)
+ann.fit(x_train, y_train, batch_size = 32, epochs = 50)
 
 train_mse = ann.evaluate(x_train, y_train, verbose = 0)
 test_mse = ann.evaluate(x_test, y_test, verbose = 0)
-
-# print('Train: %.3f, Test: %.3f' % (train_mse, test_mse))
 
 
 #Predicting the Test set results
 y_pred = ann.predict(x_test)
 
-user_input = input("Which team do you want to predict: ")
-goals_against = input("How many goals will they score: ")
+#Cleaning up variables for displaying purposes
+team_name = input("Which team do you want to predict against: ")
+user_input = TeamSelect(team_name)
+goals_against = input("How many goals will the {} score: ".format(team_name))
 
 temp_prediction = ann.predict(sc.transform([[user_input, goals_against]]))
 
-
-#Cleaning up variables for displaying purposes
 string_prediction = np.ndarray.tolist(temp_prediction)
 prediction = string_prediction[0][0]
-print(type(prediction)) 
+#print(type(prediction)) 
 
-print("My Model predics the Penguins will score: {}".format(round(prediction,2)))
+print("My Model predicts the Penguins will score {} goals against the {}".format(round(prediction,2), team_name))
 
---
+++ function_lib.py
@@ -0,0 +1,42 @@
+
+
+#Dictionary for teams
+Teams = {
+  "Ducks": 0.0,
+  "Coyotes": 1.0,
+  "Bruins": 2.0,
+  "Sabres": 3.0,
+  "Flames": 4.0,
+  "Hurricanes": 5.0,
+  "Blackhawks": 6.0,
+  "Avalanche": 7.0,
+  "Blue Jackets": 8.0,
+  "Stars": 9.0,
+  "Red Wings": 10.0,
+  "Oilers": 11.0,
+  "Panthers": 12.0,
+  "Kings": 13.0,
+  "Wild": 14.0,
+  "Canadiens": 15.0,
+  "Predators": 16.0,
+  "Devils": 17.0,
+  "Islanders": 18.0,
+  "Rangers": 19.0,
+  "Senators": 20.0,
+  "Flyers": 21.0,
+  #not sure if the pens should be here
+  "Sharks": 22.0,
+  "Kraken": 23.0,
+  "Blues": 24.0,
+  "Lightning": 25.0,
+  "Maple Leafs": 26.0,
+  "Canucks": 27.0,
+  "Golden Knights": 28.0,
+  "Capitals": 29.0,
+  "Jets": 30.0
+
+}
+
+def TeamSelect(team):
+    return Teams[team]
+
