fdb11d71b1556813365d547e6124740f0d3cb43c
Krishna <kmr614@gmail.com>
2022-11-10 20:20:00 -0500

    created function to get average goals scored by opponent and use that in ML calculation

--- function_lib.py
+++ function_lib.py
@@ -1,3 +1,6 @@
+import csv
+import math
+CSV_LOC = '/home/ish/Projects/Python_Projects/Pens_Betting/Pens Game Data/Pens_Data.csv'
 
 
 #Dictionary for teams
@@ -37,6 +40,72 @@ Teams = {
 
 }
 
+#Dictionary for the full teams names
+Full_Teams = {
+  "Ducks": "Anaheim Ducks",
+  "Coyotes": "Arizona Coyotes",
+  "Bruins": "Boston Bruins",
+  "Sabres": "Buffalo Sabres",
+  "Flames": "Calgary Flames",
+  "Hurricanes": "Carolina Hurricanes",
+  "Blackhawks": "Chicago Blackhawks",
+  "Avalanche": "Colorado Avalanche",
+  "Blue Jackets": "Columbus Blue Jackets",
+  "Stars": "Dallas Stars",
+  "Red Wings": "Detroit Red Wings",
+  "Oilers": "Edmonton Oilers",
+  "Panthers": "Florida Panthers",
+  "Kings": "Los Angeles Kings",
+  "Wild": "Minnesota Wild",
+  "Canadiens": "Montreal Canadiens",
+  "Predators": "Nashville Predators",
+  "Devils": "New Jersey Devils",
+  "Islanders": "New York Islanders",
+  "Rangers": "New York Rangers",
+  "Senators": "Ottawa Senators",
+  "Flyers": "Philidelphia Flyers",
+  #not sure if the pens should be here
+  "Sharks": "San Jose Sharks",
+  "Kraken": "Seattle Kraken",
+  "Blues": "St. Louis Blues",
+  "Lightning": "Tampa Bay Lightning",
+  "Maple Leafs": "Toronto Maple Leafs",
+  "Canucks": "Vancouver Canucks",
+  "Golden Knights": "Vegas Golden Knights",
+  "Capitals": "Washington Capitals",
+  "Jets": "Winnapeg Jets"
+
+}
+
+#Funtions for grabing the team names
+
 def TeamSelect(team):
     return Teams[team]
+def FullTeamSelect(team):
+    return Full_Teams[team]
 
+def GetAverageGoalsAgainst(team):
+  #go through every game the other team has gotten and get the avearage goals scored by them.
+  #that will be used as input for the calculation.
+  count = 0
+  average_goals = 0
+  total_goals = 0
+  goals_list = []
+
+  team_agaisnt = FullTeamSelect(team)
+
+
+  with open(CSV_LOC) as f:
+      reader = csv.reader(f)
+      for row in reader:
+        if row[0] == team_agaisnt:
+          total_goals += int(row[2])
+          goals_list.append(row[2])
+  
+
+  average_goals = total_goals / len(goals_list)
+  floor_average_goals = math.floor(average_goals) 
+  return floor_average_goals
+          
+
+#GetAverageGoalsAgainst('Ducks')
